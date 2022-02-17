29c47a03742eb24e624e3c91e169d2daac14f68e
Molnár Attila <molnar.attila@szlgbp.hu>
2022-02-17 07:34:04 +0100

    fraktálokkal teszt

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -27,11 +27,20 @@ namespace LogoKaresz
 			defaultkaresz = new Avatar(this, new Pont(400, 400), 90);
             timer1.Interval = 5;
             timer2.Interval = 5;
-		}
+            Feladateloszto();
+        }
 
-		private void startgomb_Click(object sender, EventArgs e)
+        private void startgomb_Click(object sender, EventArgs e)
 		{
-			FELADAT();
+            int melyikfeladat = (int)Feladatvalaszto.Value;
+            if (0 <= melyikfeladat && melyikfeladat < feladatok.Length)
+            {
+                feladatok[melyikfeladat].Invoke();
+            }
+            else
+            {
+                MessageBox.Show("nincs ilyen indexű feladat regisztrálva. A Feladateloszto függvényben tudsz új függvényeket regisztrálni.");
+            }
 		}
 
 
@@ -67,7 +76,9 @@ namespace LogoKaresz
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Feladatvalaszto = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.képkeret)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Feladatvalaszto)).BeginInit();
             this.SuspendLayout();
             // 
             // képkeret
@@ -95,7 +106,7 @@ namespace LogoKaresz
             // dlx
             // 
             this.dlx.AutoSize = true;
-            this.dlx.Location = new System.Drawing.Point(754, 68);
+            this.dlx.Location = new System.Drawing.Point(754, 102);
             this.dlx.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.dlx.Name = "dlx";
             this.dlx.Size = new System.Drawing.Size(35, 13);
@@ -105,7 +116,7 @@ namespace LogoKaresz
             // dly
             // 
             this.dly.AutoSize = true;
-            this.dly.Location = new System.Drawing.Point(754, 93);
+            this.dly.Location = new System.Drawing.Point(754, 127);
             this.dly.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.dly.Name = "dly";
             this.dly.Size = new System.Drawing.Size(35, 13);
@@ -115,7 +126,7 @@ namespace LogoKaresz
             // dli
             // 
             this.dli.AutoSize = true;
-            this.dli.Location = new System.Drawing.Point(754, 120);
+            this.dli.Location = new System.Drawing.Point(754, 154);
             this.dli.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.dli.Name = "dli";
             this.dli.Size = new System.Drawing.Size(35, 13);
@@ -124,7 +135,7 @@ namespace LogoKaresz
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(763, 165);
+            this.button1.Location = new System.Drawing.Point(763, 199);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(29, 32);
             this.button1.TabIndex = 5;
@@ -135,7 +146,7 @@ namespace LogoKaresz
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(798, 165);
+            this.button2.Location = new System.Drawing.Point(798, 199);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(29, 32);
             this.button2.TabIndex = 6;
@@ -147,7 +158,7 @@ namespace LogoKaresz
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(777, 149);
+            this.label1.Location = new System.Drawing.Point(777, 183);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 7;
@@ -161,11 +172,19 @@ namespace LogoKaresz
             // 
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
+            // Feladatvalaszto
+            // 
+            this.Feladatvalaszto.Location = new System.Drawing.Point(755, 55);
+            this.Feladatvalaszto.Name = "Feladatvalaszto";
+            this.Feladatvalaszto.Size = new System.Drawing.Size(85, 20);
+            this.Feladatvalaszto.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(848, 459);
+            this.Controls.Add(this.Feladatvalaszto);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -178,6 +197,7 @@ namespace LogoKaresz
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.képkeret)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Feladatvalaszto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -204,6 +224,7 @@ namespace LogoKaresz
         private Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
-	}
+        private NumericUpDown Feladatvalaszto;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,81 @@
 using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-		/* Függvények */
-
-
-		/* Függvények vége */
-		void FELADAT()
+		void fodros_fraktál(int év, double méret)
 		{
-			/* Ezt indítja a START gomb! */
-			// Teleport(közép.X, közép.Y+150, észak);
+			if (év == 1)
+			{
+				Előre(méret);
+				Jobbra(60);
+				Előre(méret);
+				Balra(120);
+				Előre(2 * méret);
+				Jobbra(120);
+				Előre(méret);
+				Balra(60);
+			}
+			else if (év > 1)
+			{
+				fodros_fraktál(év - 1, méret / 3);
+				Jobbra(60);
+				fodros_fraktál(év - 1, méret / 3);
+				Balra(120);
+				fodros_fraktál(év - 1, méret / 3);
+				fodros_fraktál(év - 1, méret / 3);
+				Jobbra(120);
+				fodros_fraktál(év - 1, méret / 3);
+				Balra(60);
+			}
+		}
+
+		
+		void szigetj(int n, int év, double méret)
+		{
+            for (int i = 0; i < n; i++)
+            {
+				fodros_fraktál(év, méret);
+				Jobbra(360/(double)n);
+            }
+		}
+		void szigete(int n, int év, double méret)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				fodros_fraktál(év, méret);
+				Balra(360 / (double)n);
+			}
+		}
+
+		Action[] feladatok;
+		void Feladateloszto() 
+		{
+			feladatok = new Action[] 
+			{	
+				FELADAT0,
+				FELADAT1,
+			};
+		}
+
+		void FELADAT0()
+		{
+            using (new Frissítés(false))
+            {
+				szigetj(6, 5, 20);
+            }
+		}
+
+		void FELADAT1()
+		{
+			using (new Frissítés(false))
+			{
+				szigete(6, 5, 20);
+			}
 		}
 	}
 }
